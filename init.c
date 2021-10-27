a33d84ebfd274936bab96ae0eb70fb8cb6686004
TrsVu <tristan.vu@gmail.com>
2021-10-27 12:53:34 +0200

    ncurse2

--- init.c
+++ init.c
@@ -31,9 +31,10 @@ ENNEMI init_ennemi(char D,char* nom_fic,int x, int y,int length,int largeur)
 	E.posy = y;
 	E.direction = D;
 	E.vitesse = 5000;
-	E.etat = 'I';
+	E.etat = 'V';
 	E.timing=0;
 	E.largeur=largeur;
+	E.blindage=3;
 	
 	int* vaisseau=chargement(nom_fic,E.length);
 	E.skin=getSkin(vaisseau,E.length);
@@ -47,22 +48,50 @@ MISSILE init_missile(char D,int x, int y,char etat)
 	M.posx = x;
 	M.posy = y;
 	M.direction = D;
-	M.vitesse = 10000;
+	M.vitesse = 6000;
 	M.skin="¤";
 	M.etat = etat;
 	M.timing=0;
 	return M;
 }
 
+ETOILE init_etoile()
+{
+	ETOILE E;
+	E.posx = 1+(rand()%36);
+	E.posy = 1+(rand()%70);
+	E.vitesse = 8000;
+	E.skin="*";
+	E.etat = 'V';
+	E.timing=0;
+	return E;
+}
+
 MISSILE* tabMissile(int length,JOUEUR j1)
 {
 	MISSILE* M=malloc(length*sizeof(MISSILE));
-	int i=0;
+	int i;
 	for(i=0;i<length;i++)
 	{
 		M[i]=init_missile('N',j1.posx-1-1,(j1.largeur/2)+j1.posy-2,'I');
 	}
 	return M;
+}
+
+ETOILE* tabEtoile(int length)
+{
+	ETOILE* E=malloc(length*sizeof(ETOILE));
+	int i;
+	for(i=0;i<length;i++)
+	{
+		E[i]=init_etoile();
+		if(i>=length-35)
+		{
+			E[i].posx = -(1+(rand()%23));
+			E[i].posy = 1+(rand()%70);
+		}
+	}
+	return E;
 }
 
 Ptliste initListe(ENNEMI X)
--- ncurses.c
+++ ncurses.c
@@ -4,22 +4,28 @@ int main(void)
 {
 	setlocale(LC_ALL, "");
 	initscr();cbreak();noecho(),nodelay(stdscr,TRUE);
+	curs_set(0);
 	char c;
 	//int posxM=posx-1;
 	//int posyM=(5/2)+posy;
 	
-	// int timing;
+	int timing=0;
+	int vitesse=100000;
 	int i=0;
-	int nbMissile=30;
+	int nbMissile=25;
+	int nbEtoile=55;
 	JOUEUR j1= init_joueur();
-	ENNEMI e1= init_ennemi('S',"vaisseauB2.txt",10,10,18,6);
-	ENNEMI e2= init_ennemi('S',"vaisseauB.txt",10,15,18,6);
-	ENNEMI e3= init_ennemi('S',"vaisseauB2.txt",10,20,18,6);
+	// ENNEMI e1= init_ennemi('S',"vaisseauB2.txt",10,10,18,6);
+	// ENNEMI e2= init_ennemi('S',"vaisseauB.txt",10,15,18,6);
+	// ENNEMI e3= init_ennemi('S',"vaisseauB2.txt",10,20,18,6);
+	ENNEMI e3= init_ennemi('S',"vaisseauC.txt",10,20,21,7);
 	// MISSILE m1=init_missile('N',j1.posx-1-1,(j1.largeur/2)+j1.posy-2,'I');
 	MISSILE* tabM =tabMissile(nbMissile,j1);
+	ETOILE* tabEt =tabEtoile(nbEtoile);
+	affichage_Etoile(tabEt,nbEtoile);
 	printSkinCurse(j1.skin,j1.length,j1.posx,j1.posy,j1.largeur);
-	printSkinCurse(e1.skin,e1.length,e1.posx,e1.posy,e1.largeur);
-	printSkinCurse(e2.skin,e2.length,e2.posx,e2.posy,e2.largeur);
+	// printSkinCurse(e1.skin,e1.length,e1.posx,e1.posy,e1.largeur);
+	// printSkinCurse(e2.skin,e2.length,e2.posx,e2.posy,e2.largeur);
 	printSkinCurse(e3.skin,e3.length,e3.posx,e3.posy,e3.largeur);
 	move(LINES - 1, COLS - 1);
 	while((c=getch())!=27)
@@ -27,84 +33,101 @@ int main(void)
 		
 		if(c=='s'&&j1.posx<37)
 		{
-			supprSkinCurse(j1.posx, j1.posy,c,j1.length/j1.largeur,j1.largeur);
+			// supprSkinCurse(j1.posx, j1.posy,c,j1.length/j1.largeur,j1.largeur);
 			j1.posx++;
 		}
 		if(c=='z'&&j1.posx>1)
 		{
-			supprSkinCurse(j1.posx, j1.posy,c,j1.length/j1.largeur,j1.largeur);
+			// supprSkinCurse(j1.posx, j1.posy,c,j1.length/j1.largeur,j1.largeur);
 			j1.posx--;
 		}
 		if(c=='q'&&j1.posy>1)
 		{
-			supprSkinCurse(j1.posx, j1.posy,c,j1.length/j1.largeur,j1.largeur);
+			// supprSkinCurse(j1.posx, j1.posy,c,j1.length/j1.largeur,j1.largeur);
 			j1.posy--;
 		}
 		if(c=='d'&&j1.posy<60)
 		{
-			supprSkinCurse(j1.posx, j1.posy,c,j1.length/j1.largeur,j1.largeur);
+			// supprSkinCurse(j1.posx, j1.posy,c,j1.length/j1.largeur,j1.largeur);
 			j1.posy++;
 		}
-		printSkinCurse(j1.skin,j1.length,j1.posx,j1.posy,j1.largeur);
 		
 		
-		for(int j=0;j<nbMissile;j++)
-		{
-			if(tabM[j].etat=='V')
-			{
-				tabM[j].timing = (tabM[j].timing+1)%tabM[j].vitesse;
-				if(tabM[j].timing==100)
-					tabM[j].posx--;
-				mvprintw(tabM[j].posx,tabM[j].posy,"%s",tabM[j].skin);
-				mvprintw(tabM[j].posx+1,tabM[j].posy," ");
-				if(tabM[j].posx==0)
-				{
-					tabM[j].etat='I';
-				}
-				if(tabM[j].etat=='I')
-				{
-					mvprintw(tabM[j].posx,tabM[j].posy," ");
-				}
-			}
-		}
-		if(c=='p')
+		
+		
+		timing=(timing+1)%vitesse;
+		if(c=='p'&&timing>=20000)
 		{
 			tabM[i].timing=0;
+			timing=0;
 			tabM[i]=init_missile('N',j1.posx-1-1,(j1.largeur/2)+j1.posy-2,'V');
 			if(i<=nbMissile-2)
 				i++;
 			else
 				i=0;
 		}
+		//AFFICHAGE
+		erase();
+		tabEt[1].timing = (tabEt[1].timing+1)%tabEt[1].vitesse;
+		for(int j=0;j<nbEtoile;j++)
+		{
+			// if(tabEt[j].etat=='V')
+			// {
+				if(tabEt[1].timing==100)
+					tabEt[j].posx++;
+				mvprintw(tabEt[j].posx,tabEt[j].posy,"*");
 			// }
-		/*
-		if(m1.etat=='V')
-		{
-			timing = (timing+1)%m1.vitesse;
-			if(timing==100)
+			if(tabEt[j].posx>=60)
 			{
-				m1.posx--;
-			}
-			// timeout(500);
-			//napms(500);
-			mvprintw(m1.posx+1,m1.posy," ");
-			mvprintw(m1.posx,m1.posy,"%s",m1.skin);
-			if(m1.posx==0)
-			{
-				mvprintw(m1.posx,m1.posy," ");
-				m1=init_missile('N',j1.posx-1-1,(j1.largeur/2)+j1.posy-2,'I');
+				tabEt[j].posx = 1;
+				tabEt[j].posy = 1+(rand()%59);
 			}
 		}
-		else
+		// affichage_Etoile(tabEt,nbEtoile);
+		printSkinCurse(j1.skin,j1.length,j1.posx,j1.posy,j1.largeur);
+		
+		if(e3.etat=='V')
+			printSkinCurse(e3.skin,e3.length,e3.posx,e3.posy,e3.largeur);
+		tabM[1].timing = (tabM[1].timing+1)%tabM[1].vitesse;
+		for(int j=0;j<nbMissile;j++)
 		{
-			timing=0;
-			if(c=='p')
+			if(tabM[j].etat=='V')
 			{
-				m1=init_missile('N',j1.posx-1-1,(j1.largeur/2)+j1.posy-2,'V');
+				// tabM[j].timing = (tabM[j].timing+1)%tabM[j].vitesse;
+				
+				
+				if(tabM[1].timing==100)
+					tabM[j].posx--;
+				mvprintw(tabM[j].posx,tabM[j].posy,"%s",tabM[j].skin);
+				// mvprintw(tabM[j].posx+1,tabM[j].posy," ");
+				
+				if(hitBox(e3,tabM[j])&&e3.etat=='V')
+				{
+					if(e3.blindage!=0)
+					{
+						e3.blindage--;
+						tabM[j].etat='I';
+						mvprintw(tabM[j].posx,tabM[j].posy," ");
+					}
+					if(e3.blindage==0)
+					{
+						e3.etat='I';
+						// supprSkinCurse(e3.posx, e3.posy,'s', e3.length/e3.largeur,e3.largeur);
+						// supprSkinCurse(e3.posx+1, e3.posy,'s', e3.length/e3.largeur,e3.largeur);
+						// supprSkinCurse(e3.posx+2, e3.posy,'s', e3.length/e3.largeur,e3.largeur);
+					}
+				}
+				if(tabM[j].posx==0)
+				{
+					tabM[j].etat='I';
+					mvprintw(tabM[j].posx,tabM[j].posy," ");
+				}
 			}
 		}
-		*/
-		//printw("%d %d",j1.posx,j1.posy);
+		
+		
+		
+		
 		move(LINES - 1, COLS - 1);
 		
 	}
